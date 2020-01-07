613a9097ed2c5c0f6436a6cc86474f7ba1663934
Arpan <arpnagar1@gmail.com>
2020-01-07 20:04:06 +0530

    Navya, finish this.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,54 @@
-var name = document.querySelector('input[name="name"]').value;
-var email = document.querySelector('input[name="name"]').value;
-var tel = document.querySelector('input[name="name"]').value;
-var text_about = document.querySelector('input[name="name"]').value;
+window.addEventListener('load', function () {
+    var submit_btn = document.getElementById('submit');
+    console.log('h1');
+    submit_btn.addEventListener('click', function (e) {
+    e.preventDefault();
+    var name = document.querySelector('#form > input[name="name"]').value;
+    var email = document.querySelector('#form > input[name="email"]').value;
+    var tel = document.querySelector('input[name="tel"]').value;
+    var text_about = document.querySelector('input[name="about"]').value;
+    var year = document.querySelector('input[name="year-study"]');
+    var year_study;
+    console.log(name);
+    for(var i=0;i<year.length;i=i+1)
+    {
+        if(year[i].checked){
+            year_study = year[i].value;
+        }
+    }
+    
+    data = {
+        "name":name,
+        "email":email,
+        "phone_no":tel,
+        "about":text_about,
+        "year":year_study
+    }
+    
+    console.log(data);
+    
+    fetch('/route',{
+        method: 'POST',
+        headerheaders: {
+            "Content-Type": "application/JSON",
+        },
+        credentials: 'include',
+                    body: JSON.stringify(data),
+                    redirect: 'error'
+    }).then(res =>{
+        return res.json();
+    })
+    .then( json => {
+        if(!json.success) alert(json.msg);
+        else
+        {
+            var thank = document.getElementById('thanks');
+            thank.innerHTML='Thank you for registering!';
+        }
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+    
+    });
+});
