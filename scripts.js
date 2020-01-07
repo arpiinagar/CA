d6c4ad65fba48daf02b6418de9e2fd0a1adb56f1
Arpan Nagar <43674909+arpiinagar@users.noreply.github.com>
2020-01-07 13:24:07 +0530

    First Commit

---
+++ scripts.js
@@ -0,0 +1,4 @@
+var name = document.querySelector('input[name="name"]').value;
+var email = document.querySelector('input[name="name"]').value;
+var tel = document.querySelector('input[name="name"]').value;
+var text_about = document.querySelector('input[name="name"]').value;
